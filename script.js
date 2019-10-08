4f630f1529b8b171f855b024f0a0d44a6b8e108d
sarahz2476 <56265188+sarahz2476@users.noreply.github.com>
2019-10-08 11:02:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Sarah Zhang"
+let userName = "sarahz2476";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
